8b11abc2e3ef6a5f08f0ee490490341b2765df93
fnavarro <facunava92@gmail.com>
2020-03-18 10:06:46 -0300

    Actualizo

---
+++ Tecnicas Digitales IV/Practicos/IE_TDIV_TP2_VGA/TD4_TP2_VGA/TD4_TP2_VGA.srcs/sources_1/new/TOP_VGA.vhd
@@ -0,0 +1,40 @@
+----------------------------------------------------------------------------------
+-- UNIVERSIDAD TECNOLÓGICA NACIONAL - FACULTAD REGIONAL CÓRDOBA.
+-- CARRERA: INGENIERIA ELECTRÓNICA.
+-- Asignatura: TECNICAS DIGITALES IV (ELECTIVA).
+-- Año: 2020.
+-- Grupo n°: 
+-- Integrantes:
+--
+--      NAVARRO, Facundo    Leg. n°: 63809
+--
+-- Fecha de Entrega:
+-- Hardware Utilizado: Basys3
+-- VHDL auxiliares utilizados:
+--
+--
+-- UCF Utilizado: 
+--
+-- 
+-- Practico n°2: Generador de señal de video VGA
+--
+----------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+--use UNISIM.VComponents.all;
+
+entity TOP_VGA is
+-- Port ( 
+    );
+end TOP_VGA;
+
+architecture arch of TOP_VGA is
+
+begin
+
+
+end arch;
